6eddd195d3cf8060e74dd6060c70ea14f9399980
ItzIgor <silva.igor.014@gmail.com>
2026-03-30 08:45:03 -0300

    criação da pasta de componentes

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Input from './components/Input';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to stdsfsdfsdfsdap!</Text>
+      <Input placeholder="Email" keyboardType="email-address"/>
+      <Input 
+        placeholder="Senha" 
+        secureTextEntry={true}
+        autoCapitalize="none"
+        autoComplete="password"
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +25,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20, 
   },
 });
---
+++ components/Input.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { TextInput, View, StyleSheet } from 'react-native';
+
+export default function Input({placeholder, ...props }) {
+  return (
+    <View style={styles.container}>
+      <TextInput  style={styles.inputBox} placeholder={placeholder} {...props}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  inputBox: {
+    borderBlockColor: "black",
+    borderWidth: 2,
+    padding: 5,
+    borderRadius: 10
+  }
+  
+});
